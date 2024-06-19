5ebeca225cf6efabb116a730657bcfda4348d19a
Tom <tom@isendu.com>
2024-06-19 18:16:35 +0200

    chore(Dockerfile): update COPY path for commons directory to match new project structure refactor(main.py): refactor startup event handling to use FastAPI's on_startup parameter for better readability and maintainability

--- server/main.py
+++ server/main.py
@@ -1,15 +1,16 @@
 import asyncio
-
+from contextlib import asynccontextmanager
 from server.routers import ws
 from server.routers import manager
-from fastapi import FastAPI, BackgroundTasks
+from fastapi import FastAPI
 
-app = FastAPI()
+
+@asynccontextmanager
+def on_startup():
+    asyncio.create_task(ws.ping_agents())
+
+
+app = FastAPI(on_startup=[on_startup])
 
 app.include_router(ws.router)
 app.include_router(manager.router)
-
-
-@app.on_event("startup")
-async def startup_event():
-    await asyncio.create_task(ws.ping_agents())
